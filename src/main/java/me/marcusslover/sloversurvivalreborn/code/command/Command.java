dca0963027408bc8def2ced7a922ab6b27370403
MarcusSlover <mariuszek016@gmail.com>
2021-03-02 20:12:47 +0100

    + More command options + Warp Command

--- src/main/java/me/marcusslover/sloversurvivalreborn/code/command/Command.java
+++ src/main/java/me/marcusslover/sloversurvivalreborn/code/command/Command.java
@@ -4,9 +4,12 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.Collections;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Command {
     String name();
+    String description() default "";
+    String[] aliases() default {};
 }
